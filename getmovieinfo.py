ec375e9436778d0a9ab45b4b8ad9375aa56dace2
nixeneko <nixeneko@users.noreply.github.com>
2021-07-02 10:19:53 +0900

    ソード時に無視する記号の追加

--- getmovieinfo.py
+++ getmovieinfo.py
@@ -100,7 +100,7 @@ def remove_prefix(s): #先頭の"劇場版", "映画"等はソート時には無
     return re.sub(r"^(劇場版|映画|（旧作）|\(旧作\))", "", s).strip()
     
 def remove_signs(s):
-    return re.sub(r"[「」『』、。：・/／－—−\-～〜~]", "", s)
+    return re.sub(r"[「」『』、。：・/／－—−–\-～〜~]", "", s)
 
 def hira_to_kata(s): #http://python-remrin.hatenadiary.jp/entry/2017/04/26/123458
     return "".join([chr(ord(c) + 96) if ("ぁ" <= c <= "ゖ") else c for c in s])
